d217e67f18bb7a5334a442c6879c7eacaaad0026
nnyx7 <nnyx77@gmail.com>
2021-02-21 02:39:06 +0200

    Add game over screen

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@ mod block;
 mod board;
 mod event;
 mod layout_manager;
+mod screens;
 
 use board::Board;
-use layout_manager::get_layouts;
-
 use event::{Event, Events};
+use layout_manager::get_layouts;
+use screens::game_over;
 use std::error::Error;
 use std::io;
 use termion::{event::Key, raw::IntoRawMode, screen::AlternateScreen};
@@ -28,17 +29,23 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     loop {
         terminal.draw(|f| {
-            let layouts = get_layouts(f.size());
+            if !board.has_game_ended() {
+                let layouts = get_layouts(f.size());
 
-            f.render_widget(
-                board.clone(),
-                *layouts.get(&"first_board".to_string()).unwrap(),
-            );
+                f.render_widget(
+                    board.clone(),
+                    *layouts.get(&"first_board".to_string()).unwrap(),
+                );
+            } else {
+                let screen = game_over();
+                f.render_widget(screen, f.size())
+            }
         })?;
 
         if let Event::Input(key) = events.next()? {
             match key {
                 Key::Char('q') => break,
+                Key::Char('r') => board = Board::default(),
                 _ => board.make_action(&key),
             }
         };
